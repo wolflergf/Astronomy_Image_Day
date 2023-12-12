71225090f16925c1e6566809959180ac7536a166
wolflergf <wolflergf@gmail.com>
2023-12-12 09:22:38 +0000

    improve the code

--- get_image.py
+++ get_image.py
@@ -2,20 +2,35 @@ import requests
 from dotenv import load_dotenv
 import os
 
+# Load environment variables from .env file
 load_dotenv()
+
+# Get API key from environment variables
 my_key = os.getenv('API_KEY')
 
+# Define API endpoint
 END_POINT = 'https://api.nasa.gov/planetary/apod'
+
+# Define parameters for API request
 image_params = {
     'api_key': my_key,
     'count': 1
 }
 
 def get_image():
+    # Send GET request to API endpoint with defined parameters
     response = requests.get(END_POINT, params=image_params)
+    
+    # Raise exception if request was unsuccessful
     response.raise_for_status()
+    
+    # Parse response data as JSON
     data = response.json()
+    
+    # Extract title, explanation, and URL from response data
     title = (data[0]['title'])
     explanation = (data[0]['explanation'])
     url = (data[0]['hdurl'])
+    
+    # Return title, explanation, and URL
     return title, explanation, url
--- main.py
+++ main.py
@@ -1,9 +1,17 @@
+# Import the streamlit library for creating web apps
 import streamlit as st
+
+# Import the get_image function from the get_image module
 from get_image import get_image
 
+# Call the get_image function and unpack its return values into title, explanation, and url
 title, explanation, url = get_image()
 
+# Use streamlit to create a title on the web page with the title from get_image
 st.title(title)
-st.image(url)
-st.write(explanation)   
 
+# Use streamlit to display an image on the web page with the url from get_image
+st.image(url)
+
+# Use streamlit to write the explanation from get_image on the web page
+st.write(explanation)
